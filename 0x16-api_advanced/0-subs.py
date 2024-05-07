f51682049f527f65b0952d664d281fa123266efa
Remo3311 <remo33553311@gmail.com>
2024-05-07 18:17:14 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+    return subs
 """
 Contains the number_of_subscribers function
 """
@@ -10,8 +11,11 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    try:
+        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                         headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+        subs = r.get("data", {}).get("subscribers", 0)
+        return subs
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
+        return 0
